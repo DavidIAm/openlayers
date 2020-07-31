22dafedb3838a71685404de6ec86c72e18123a26
David Ihnen <davidihnen@gmail.com>
2020-07-30 21:18:02 -0400

    Don't send the same array multiple times
    Calling the callback with the same array over and over, only with its elements reassigned, led to some tricky bugs to track.  By formulating new arrays in the callback call, we avoid having the same array reference presented to the callback on every iteration, so it can do what it likes with its very own copy.

--- src/ol/geom/flat/segments.js
+++ src/ol/geom/flat/segments.js
@@ -1,7 +1,6 @@
 /**
  * @module ol/geom/flat/segments
  */
-
 /**
  * This function calls `callback` for each segment of the flat coordinates
  * array. If the callback returns a truthy value the function returns that
@@ -16,18 +15,19 @@
  * @template T
  */
 export function forEach(flatCoordinates, offset, end, stride, callback) {
-  const point1 = [flatCoordinates[offset], flatCoordinates[offset + 1]];
-  const point2 = [];
-  let ret;
-  for (; offset + stride < end; offset += stride) {
-    point2[0] = flatCoordinates[offset + stride];
-    point2[1] = flatCoordinates[offset + stride + 1];
-    ret = callback(point1, point2);
-    if (ret) {
-      return ret;
+    var point1 = [flatCoordinates[offset], flatCoordinates[offset + 1]];
+    var point2 = [];
+    var ret;
+    for (; (offset + stride) < end; offset += stride) {
+        point2[0] = flatCoordinates[offset + stride];
+        point2[1] = flatCoordinates[offset + stride + 1];
+        ret = callback([...point1], [...point2]);
+        if (ret) {
+            return ret;
+        }
+        point1[0] = point2[0];
+        point1[1] = point2[1];
     }
-    point1[0] = point2[0];
-    point1[1] = point2[1];
-  }
-  return false;
+    return false;
 }
+//# sourceMappingURL=segments.js.map
